05865767c9884d419b50a7622ae07e8ad067a24f
Yusuke Yamamoto <yusuke@mac.com>
2023-06-04 10:48:34 +0900

    initial commit

---
+++ src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.samuraism.fizzbuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+    }
+
+    public static String fizzBuzz(int number) {
+        return null;
+    }
+}
