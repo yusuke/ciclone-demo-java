05917231bc5f94d57c1d495a39e27184fbb7ffe9
Yusuke Yamamoto <yusuke@mac.com>
2023-09-06 12:45:22 +0900

    リファクタリング

--- src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
@@ -9,17 +9,17 @@ public class FizzBuzz {
 
     public static String fizzBuzz(int number) {
         String result = "";
-        if (number % 3 == 0) {
-            result += "Fizz";
-        }
-        if (number % 5 == 0) {
-            result += "Buzz";
-        }
-
-        if (number % 3 != 0 && number % 5 != 0) {
-            result = String.valueOf(number);
-
-        }
+//        if (number % 3 == 0) {
+//            result += "Fizz";
+//        }
+//        if (number % 5 == 0) {
+//            result += "Buzz";
+//        }
+//
+//        if (number % 3 != 0 && number % 5 != 0) {
+//            result = String.valueOf(number);
+//
+//        }
         return result;
     }
 }
